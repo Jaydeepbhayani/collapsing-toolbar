4aadf103a4c9942e07b03b1d28b889e43f43145e
Jaydeep Bhayani <jaydeepbhayani773@gmail.com>
2018-03-31 12:52:03 +0300

    framelayout added...

--- app/src/main/java/anetos/software/collapsing_toolbar/MainActivity.java
+++ app/src/main/java/anetos/software/collapsing_toolbar/MainActivity.java
@@ -5,10 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,7 +16,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private RelativeLayout rl1, rl2;
+    //private RelativeLayout rl1, rl2;
+    private LinearLayout rl1, rl2;
     private LinearLayout content1, content2;
     private CardView cardView1, cardView2, cardView3;
     private BottomNavigationView mBottomNavigationView;
@@ -27,8 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
 
         setupBottomNavigation();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
